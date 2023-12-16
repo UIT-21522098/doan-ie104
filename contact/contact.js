b5004ecb2d508e803223c44b297cbf6f32de2038
UIT.21520138 <21520138@gm.uit.edu.vn>
2023-12-16 20:32:13 +0700

    add contact

---
+++ contact/contact.js
@@ -0,0 +1,8 @@
+function gui(){
+    let name=document.getElementById("name").value;
+    let email=document.getElementById("email").value;
+    let phone=document.getElementById("phone").value;
+    let address=document.getElementById("address").value;
+    let message=document.getElementById("message").value;
+    alert("Cảm ơn "+ name + " đã liên lạc cho Nook&Nest. Chúng tôi sẽ phản hồi trong thời gian sớm nhất!")
+}
